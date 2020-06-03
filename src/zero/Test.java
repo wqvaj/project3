e1dcda86c8552944ae8b6a3c4aa1dd404e564a06
wq <wq@qq.com>
2020-06-03 14:08:56 +0800

    asdfas

--- src/zero/Test.java
+++ src/zero/Test.java
@@ -4,9 +4,18 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class Test {
 		public static void main(String[] args) {
-				System.out.println(123);
+				Set<String> set = new HashSet<String>();
+				set.add("r");
+				set.add("b");
+				set.add("c");
+				set.add("d");
+				Iterator<String> it = set.iterator();
+				for(String s:set){
+					System.out.println(s);
+				}
 		}		
 }
