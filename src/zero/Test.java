4146380f14a6201c59bf5904a2b3ba40612a9633
wq <wq@qq.com>
2020-06-03 11:36:23 +0800

    adfasd

--- src/zero/Test.java
+++ src/zero/Test.java
@@ -7,15 +7,6 @@ import java.util.List;
 
 public class Test {
 		public static void main(String[] args) {
-		HashSet<String> set = new HashSet<String>();
-		set.add("a");
-		set.add("b");
-		set.add("B");
-		set.add("c");
-		set.add("d");
-		Iterator<String> iterator = set.iterator();
-		while(iterator.hasNext()){
-			System.out.println(iterator.next());
-		}
+				System.out.println(123);
 		}		
 }
